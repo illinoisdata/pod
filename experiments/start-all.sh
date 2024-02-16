7bdaf01e038fb64a59fe919a3b593af75a1db8fd
Supawit Chockchowwat <supawitch22@gmail.com>
2024-02-16 16:25:06 -0600

    Specify compose name to allow multiple simultaneous composes

--- experiments/start-all.sh
+++ experiments/start-all.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]
+then
+    echo "Require 1 argument (COMPOSE_NAME), $# provided"
+    echo 'Example: bash experiments/start-all.sh pod_${USER}'
+    exit 1
+fi
+
+COMPOSE_NAME=$1
+echo "Using COMPOSE_NAME= ${COMPOSE_NAME}"
+sleep 3
+
 docker build -t pod -f experiments/pod.Dockerfile . && \
-    docker compose -f experiments/docker-compose.yml up
+    docker compose -f experiments/docker-compose.yml -p ${COMPOSE_NAME} up
