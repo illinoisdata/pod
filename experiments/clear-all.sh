dee04721fc0d3a88a2d100df343c4173c0662c74
Supawit Chockchowwat <supawitch22@gmail.com>
2024-02-16 16:25:06 -0600

    Specify compose name to allow multiple simultaneous composes

--- experiments/clear-all.sh
+++ experiments/clear-all.sh
@@ -1,2 +1,14 @@
-docker compose -f experiments/docker-compose.yml down
-docker compose -f experiments/docker-compose.yml rm -f -s -v
+#!/bin/bash
+if [ "$#" -ne 1 ]
+then
+    echo "Require 1 argument (COMPOSE_NAME), $# provided"
+    echo 'Example: bash experiments/clear-all.sh pod_${USER}'
+    exit 1
+fi
+
+COMPOSE_NAME=$1
+echo "Using COMPOSE_NAME= ${COMPOSE_NAME}"
+sleep 3
+
+docker compose -f experiments/docker-compose.yml -p ${COMPOSE_NAME} down
+docker compose -f experiments/docker-compose.yml -p ${COMPOSE_NAME} rm -f -s -v
