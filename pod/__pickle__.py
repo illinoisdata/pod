e1bb134677e5473ec8910e11b585c37eb7b4ce75
Supawit Chockchowwat <supawitch22@gmail.com>
2024-03-03 00:03:42 -0600

    Set higher recursion limit

--- pod/__pickle__.py
+++ pod/__pickle__.py
@@ -1,4 +1,5 @@
 import pickle
+import sys
 
 # Use Python pickle for testing pod prototype. When ideas are solidified and pod is implemented in C, revert this back.
 pickle.Pickler = pickle._Pickler  # type: ignore
@@ -7,3 +8,6 @@ pickle.dump = pickle._dump  # type: ignore
 pickle.dumps = pickle._dumps  # type: ignore
 pickle.load = pickle._load  # type: ignore
 pickle.loads = pickle._loads  # type: ignore
+
+
+sys.setrecursionlimit(100000)
