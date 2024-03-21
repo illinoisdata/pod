c8f78d1ab0435cdb21dde5d26c863759dbb9ba12
Sumay Thakurdesai <sumayst2@yongjoo-serv-01.cs.illinois.edu>
2024-03-21 09:03:34 -0500

    train params current

--- experiments/train.sh
+++ experiments/train.sh
@@ -1,9 +1,12 @@
-for alpha in 0.1 0.2 0.3 0.4 0.5;
-do 
-    for gamma in 0.6 0.7 0.8 0.9 1.0;
-        do
-            echo "alpha $alpha gamma $gamma"
-            ( docker exec experiments-pod-1 python pod/train.py --gamma $gamma --alpha $alpha & )
-        done
+for alpha in 0.1; do 
+    for gamma in 0.7; do
+        echo "alpha $alpha gamma $gamma"
+        stdout_file="eval_logs/stdout_alpha${alpha}_gamma${gamma}.log"
+        stderr_file="eval_logs/stderr_alpha${alpha}_gamma${gamma}.log"
+        # Ensure the train_logs directory exists
+        mkdir -p eval_logs
+        # Execute the command and use tee for stdout and stderr
+        ( docker exec experiments-pod-1 python pod/train.py --gamma $gamma --alpha $alpha 2> >(tee "$stderr_file" >&2) | tee "$stdout_file" ) &
+    done
+done
 echo "done with script"
-done;
